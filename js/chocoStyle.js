afaf309c320d0595d9d9dfd3b672fc661ccfc80e
James <jaimea111@gmail.com>
2018-02-26 04:49:55 -0500

    JS function to recognize fab clicks

--- js/chocoStyle.js
+++ js/chocoStyle.js
@@ -1,3 +1,10 @@
 function chocoRedirect(){
 	 $('ul.tabs').tabs('select_tab', 'tab_id');
 }
+function selection(a){
+	if(a==='premadePizza'){
+		alert("You will buy a prefabricated pizza");
+	} else if(a==='ownPizza'){
+		alert("You will build your own pizza");
+	}
+}
